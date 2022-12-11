367efb5420027c09ab3e93b54f8574a0dd92ed4b
mohamadmelhem <mhamadmlhm0@gmail.com>
2022-12-11 21:16:56 +0200

    Basic Queries

---
+++ answers.sh
@@ -0,0 +1,13 @@
+1 "SELECT name  FROM students"
+2 "SELECT name FROM students WHERE Age>30"
+3 "SELECT name FROM students WHERE Gender='f' AND Age=30"
+4 "SELECT Points FROM students WHERE id = 1"
+5 "INSERT INTO students (name,Age,Gender,Points)
+ VALUES('mohamad','24','m','300')"
+ 6 "UPDATE  students  
+  SET Points= 400 
+  WHERE id = 2"
+7 "UPDATE students 
+  SET Points = 100
+  WHERE id = 1"
+8 
